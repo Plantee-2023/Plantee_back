33cad76e22bcff94e23b21e29c9a996808f032a7
gwoneu <gwoneunji9634@gmail.com>
2024-01-08 20:23:37 +0900

    Feat: 식물백과 등록/수정 기능구현

--- src/main/java/com/plantee/controller/PlantRestController.java
+++ src/main/java/com/plantee/controller/PlantRestController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,15 @@ public class PlantRestController {
 	@GetMapping("/list.json")
 	public HashMap<String, Object> list(QueryVO vo) {
 		return service.list(vo);
+	}
+	
+	@PostMapping("/insert")
+	public void insert(@RequestBody PlantVO vo) {
+		dao.insert(vo);
+	}
+	
+	@PostMapping("/update")
+	public void update(@RequestBody PlantVO vo) {
+		dao.update(vo);
 	}
 }
